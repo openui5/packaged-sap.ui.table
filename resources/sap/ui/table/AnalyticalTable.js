b889097d65c733f142803cf6cc66129648ccb134
OpenUI5 <openui5@sap.com>
2015-02-04 14:57:45 +0100

    1.26.6

--- resources/sap/ui/table/AnalyticalTable.js
+++ resources/sap/ui/table/AnalyticalTable.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/analytics/TreeBindingAdapter',
 	 * @class
 	 * Table which handles analytical OData backends
 	 * @extends sap.ui.table.Table
-	 * @version 1.26.4
+	 * @version 1.26.6
 	 *
 	 * @constructor
 	 * @public
