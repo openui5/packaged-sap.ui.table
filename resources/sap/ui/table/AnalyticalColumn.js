6dd6ad1d73cb3a68d7877b76143cdb06da659bcf
OpenUI5 <openui5@sap.com>
2016-02-22 15:37:00 +0100

    1.28.29

--- resources/sap/ui/table/AnalyticalColumn.js
+++ resources/sap/ui/table/AnalyticalColumn.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './Column', './library'],
 	 * @extends sap.ui.table.Column
 	 *
 	 * @author SAP SE
-	 * @version 1.28.28
+	 * @version 1.28.29
 	 *
 	 * @constructor
 	 * @public
