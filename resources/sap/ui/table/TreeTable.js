514ff79fe11f7d3655f8b6da8a74ca8012bb81ab
OpenUI5 <openui5@sap.com>
2016-02-22 19:05:59 +0100

    1.34.7

--- resources/sap/ui/table/TreeTable.js
+++ resources/sap/ui/table/TreeTable.js
@@ -18,7 +18,7 @@ sap.ui.define(['jquery.sap.global', './Table', 'sap/ui/model/odata/ODataTreeBind
 	 * @class
 	 * The TreeTable Control.
 	 * @extends sap.ui.table.Table
-	 * @version 1.34.6
+	 * @version 1.34.7
 	 *
 	 * @constructor
 	 * @public
