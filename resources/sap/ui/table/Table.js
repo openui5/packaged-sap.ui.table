bc32df206f5c922bc23e8609d78456aeb0613dd8
OpenUI5 <openui5@sap.com>
2016-04-21 17:50:23 +0200

    1.36.8

--- resources/sap/ui/table/Table.js
+++ resources/sap/ui/table/Table.js
@@ -37,7 +37,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Interval
 	 *
 	 *
 	 * @extends sap.ui.core.Control
-	 * @version 1.36.7
+	 * @version 1.36.8
 	 *
 	 * @constructor
 	 * @public
