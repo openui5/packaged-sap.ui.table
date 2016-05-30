ede41a76606bef732b971bc5c8c03630094e0846
OpenUI5 <openui5@sap.com>
2016-05-30 18:21:56 +0200

    1.36.11

--- resources/sap/ui/table/AnalyticalColumnMenu.js
+++ resources/sap/ui/table/AnalyticalColumnMenu.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './ColumnMenu', './library'],
 	 * @extends sap.ui.table.ColumnMenu
 	 *
 	 * @author SAP SE
-	 * @version 1.36.10
+	 * @version 1.36.11
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/table/Table.js
+++ resources/sap/ui/table/Table.js
@@ -37,7 +37,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Interval
 	 *
 	 *
 	 * @extends sap.ui.core.Control
-	 * @version 1.36.10
+	 * @version 1.36.11
 	 *
 	 * @constructor
 	 * @public
