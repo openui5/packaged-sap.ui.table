a2adf93f27fce82c3e0f6a38eed7c076a4b344c0
OpenUI5 <openui5@sap.com>
2016-06-27 17:44:28 +0200

    1.36.15

--- resources/sap/ui/table/AnalyticalColumn.js
+++ resources/sap/ui/table/AnalyticalColumn.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './Column', './library'],
 	 * @extends sap.ui.table.Column
 	 *
 	 * @author SAP SE
-	 * @version 1.36.13
+	 * @version 1.36.15
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/table/AnalyticalTable.js
+++ resources/sap/ui/table/AnalyticalTable.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', './AnalyticalColumn', './Table', './TreeTabl
 	 * @see http://scn.sap.com/docs/DOC-44986
 	 *
 	 * @extends sap.ui.table.Table
-	 * @version 1.36.13
+	 * @version 1.36.15
 	 *
 	 * @constructor
 	 * @public
