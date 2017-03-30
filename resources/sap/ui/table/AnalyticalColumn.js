cc7b58136efc5f784dfe03e28942dfd57ac62117
OpenUI5 <openui5@sap.com>
2017-03-30 17:18:11 +0200

    1.28.45

--- resources/sap/ui/table/AnalyticalColumn.js
+++ resources/sap/ui/table/AnalyticalColumn.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './Column', './library'],
 	 * @extends sap.ui.table.Column
 	 *
 	 * @author SAP SE
-	 * @version 1.28.44
+	 * @version 1.28.45
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/table/TreeTable.js
+++ resources/sap/ui/table/TreeTable.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', './Table', 'sap/ui/model/odata/ODataTreeBind
 	 * @class
 	 * The TreeTable Control.
 	 * @extends sap.ui.table.Table
-	 * @version 1.28.44
+	 * @version 1.28.45
 	 *
 	 * @constructor
 	 * @public
