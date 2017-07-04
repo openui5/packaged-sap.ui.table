eb879cd6e21177cdf2b5cb7c2c2f8420faa973ae
OpenUI5 <openui5@sap.com>
2017-07-04 12:54:42 +0200

    1.28.47

--- resources/sap/ui/table/TablePersoController.js
+++ resources/sap/ui/table/TablePersoController.js
@@ -21,7 +21,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/ManagedObject'],
 	 * @extends sap.ui.base.ManagedObject
 	 *
 	 * @author SAP SE
-	 * @version 1.28.46
+	 * @version 1.28.47
 	 * @since 1.21.1
 	 *
 	 * @constructor
