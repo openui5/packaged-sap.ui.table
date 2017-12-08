25f9f71f0e9e46fbd061f2d56e6dab4826ab8f4f
OpenUI5 <openui5@sap.com>
2017-12-08 19:59:47 +0100

    1.44.25

--- resources/sap/ui/table/AnalyticalTable.js
+++ resources/sap/ui/table/AnalyticalTable.js
@@ -28,7 +28,7 @@ sap.ui.define(['jquery.sap.global', './AnalyticalColumn', './Table', './TreeTabl
 	 * @see http://scn.sap.com/docs/DOC-44986
 	 *
 	 * @extends sap.ui.table.Table
-	 * @version 1.44.24
+	 * @version 1.44.25
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/table/TablePointerExtension.js
+++ resources/sap/ui/table/TablePointerExtension.js
@@ -921,7 +921,7 @@ sap.ui.define(['jquery.sap.global', './TableExtension', './TableUtils', 'sap/ui/
 	 *
 	 * @extends sap.ui.table.TableExtension
 	 * @author SAP SE
-	 * @version 1.44.24
+	 * @version 1.44.25
 	 * @constructor
 	 * @private
 	 * @alias sap.ui.table.TablePointerExtension
