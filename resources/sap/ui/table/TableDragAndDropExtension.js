8647364af4eaf2d7c65bfa98d3a73e5e35487eb2
OpenUI5 <openui5@sap.com>
2018-02-14 14:04:31 +0100

    1.52.6

--- resources/sap/ui/table/TableDragAndDropExtension.js
+++ resources/sap/ui/table/TableDragAndDropExtension.js
@@ -256,7 +256,7 @@ sap.ui.define([
 	 *
 	 * @extends sap.ui.table.TableExtension
 	 * @author SAP SE
-	 * @version 1.52.5
+	 * @version 1.52.6
 	 * @constructor
 	 * @private
 	 * @alias sap.ui.table.TableDragAndDropExtension
