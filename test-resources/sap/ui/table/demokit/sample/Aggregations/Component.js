0d6c03c6d6dc1aad1dcb07a7594ca8c8cb751e1f
OpenUI5 <openui5@sap.com>
2018-05-09 12:55:29 +0200

    1.56.0

---
+++ test-resources/sap/ui/table/demokit/sample/Aggregations/Component.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+	'sap/ui/core/UIComponent'
+], function(UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.table.sample.Aggregations.Component", {
+		metadata : {
+			rootView : {
+				"viewName": "sap.ui.table.sample.Aggregations.View",
+				"type": "XML",
+				"async": true
+			},
+			dependencies : {
+				libs : [
+					"sap.ui.table",
+					"sap.ui.unified",
+					"sap.m"
+				]
+			},
+
+			config : {
+				sample : {
+					stretch : true,
+					files : [
+						"View.view.xml",
+						"Controller.controller.js"
+					]
+				}
+			}
+		}
+	});
+
+});
