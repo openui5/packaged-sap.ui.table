fc12f960cf8f829cf7503c0ff8dd0a0d03215425
OpenUI5 <openui5@sap.com>
2018-10-01 15:43:09 +0200

    1.56.11

--- resources/sap/ui/table/TableDragAndDropExtension.js
+++ resources/sap/ui/table/TableDragAndDropExtension.js
@@ -251,7 +251,7 @@ sap.ui.define([
 	 *
 	 * @extends sap.ui.table.TableExtension
 	 * @author SAP SE
-	 * @version 1.56.10
+	 * @version 1.56.11
 	 * @constructor
 	 * @private
 	 * @alias sap.ui.table.TableDragAndDropExtension
--- resources/sap/ui/table/TableKeyboardExtension.js
+++ resources/sap/ui/table/TableKeyboardExtension.js
@@ -237,7 +237,7 @@ sap.ui.define([
 	 * @class Extension for sap.ui.table.Table which handles keyboard related things.
 	 * @extends sap.ui.table.TableExtension
 	 * @author SAP SE
-	 * @version 1.56.10
+	 * @version 1.56.11
 	 * @constructor
 	 * @private
 	 * @alias sap.ui.table.TableKeyboardExtension
