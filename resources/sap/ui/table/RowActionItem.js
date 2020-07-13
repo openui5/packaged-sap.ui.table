7ce1cde458ddfe0afb9ed5de6c3ae047896160e1
OpenUI5 <openui5@sap.com>
2020-07-13 16:16:00 +0200

    1.52.42

--- resources/sap/ui/table/RowActionItem.js
+++ resources/sap/ui/table/RowActionItem.js
@@ -25,7 +25,7 @@ sap.ui.define(['sap/ui/core/Element', './library', 'sap/ui/unified/MenuItem'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.52.41
+	 * @version 1.52.42
 	 * @since 1.45.0
 	 *
 	 * @constructor
--- resources/sap/ui/table/TableKeyboardDelegate2.js
+++ resources/sap/ui/table/TableKeyboardDelegate2.js
@@ -70,7 +70,7 @@ sap.ui.define([
 	 *
 	 * @extends sap.ui.base.Object
 	 * @author SAP SE
-	 * @version 1.52.41
+	 * @version 1.52.42
 	 * @constructor
 	 * @private
 	 * @alias sap.ui.table.TableKeyboardDelegate2
