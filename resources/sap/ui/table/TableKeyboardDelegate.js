81492904eccbd7534984905157bf01ee276adbc9
OpenUI5 <openui5@sap.com>
2020-11-09 15:55:45 +0100

    1.38.48

--- resources/sap/ui/table/TableKeyboardDelegate.js
+++ resources/sap/ui/table/TableKeyboardDelegate.js
@@ -19,7 +19,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', './library', './Row', 
 	 *
 	 * @extends sap.ui.base.Object
 	 * @author SAP SE
-	 * @version 1.38.47
+	 * @version 1.38.48
 	 * @constructor
 	 * @private
 	 * @alias sap.ui.table.TableKeyboardDelegate
